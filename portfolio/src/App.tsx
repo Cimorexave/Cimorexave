c6e28fefe1c4d619c83a1adc87b8c2e7d0cd4577
unknown <sadeq.s502@gmail.com>
2022-09-05 15:08:11 +0430

    feat: updated functionality of drag contraint.

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,46 +2,60 @@ import React, { useCallback, useEffect, useState } from "react";
 import Particles from "react-particles";
 import type { Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
-import './App.css';
+import "./App.css";
 import particlesOptions from "./particles.json";
 import { ISourceOptions } from "tsparticles-engine";
 
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import CircleComponent from "./components/CircleComponent";
 
 function App() {
-    const particlesInit = useCallback(async (engine: Engine) => {
-        await loadFull(engine);
-    }, []);
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
 
-    //states
-    const [start, setStart] = useState<boolean>(false)
-    //handle start
-    const handleStart = () => {
-        setStart(true)
-    }
-    //lifecycle management
-    useEffect(() => {
-        const x = Math.floor(Math.random()*window.innerWidth).toString();
-        const y = Math.floor(Math.random()*window.innerHeight).toString();
-        //these values are passed down into the component for random positioning
-        console.log(`x:${x} , y: ${y}`);
-    }, [start])
+  //states
+  const [start, setStart] = useState<boolean>(false);
+  //handle start
+  const handleStart = () => {
+    setStart(true);
+  };
+  //lifecycle management
+  useEffect(() => {
+    const x = Math.floor(Math.random() * window.innerWidth).toString();
+    const y = Math.floor(Math.random() * window.innerHeight).toString();
+    //these values are passed down into the component for random positioning
+    console.log(`x:${x} , y: ${y}`);
+  }, [start]);
 
-    return (
-        <div>
-            <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
-            <motion.div initial={{ y: -80, opacity: 0 }} 
-            animate={{ y: start? -80: 0, opacity: start? 0: 1 }} transition={{ duration: 1, delay: 2 }} 
-            className="App text-center text-green-200 font-extrabold text-2xl m-8 ">
-                Tap <motion.div onClick={handleStart} animate={{ scale: start? 0.9: 1 }} transition={{ duration: 0.3 }}
-                className="underline relative cursor-pointer link italic inline-block here">
-                    Here
-                    </motion.div> To Start
-            </motion.div>
-            <motion.div animate={{ scale: start? 1:0 }}><CircleComponent /></motion.div>
-        </div>
-    );
+  return (
+    <div>
+      <Particles
+        options={particlesOptions as ISourceOptions}
+        init={particlesInit}
+      />
+      <motion.div
+        initial={{ y: -80, opacity: 0 }}
+        animate={{ y: start ? -80 : 0, opacity: start ? 0 : 1 }}
+        transition={{ duration: 1, delay: 2 }}
+        className="App text-center text-green-200 font-extrabold text-2xl m-8 "
+      >
+        Tap{" "}
+        <motion.div
+          onClick={handleStart}
+          animate={{ scale: start ? 0.9 : 1 }}
+          transition={{ duration: 0.3 }}
+          className="underline relative cursor-pointer link italic inline-block here"
+        >
+          Here
+        </motion.div>{" "}
+        To Start
+      </motion.div>
+      <motion.div animate={{ scale: start ? 1 : 0 }} transition={{ duration: "1000ms" }}>
+        <CircleComponent />
+      </motion.div>
+    </div>
+  );
 }
 
 export default App;
--- portfolio/src/components/CircleComponent.tsx
+++ portfolio/src/components/CircleComponent.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
-import {motion} from "framer-motion";
+//Imports
 
+//React Utils
+import { useEffect, useRef, useState } from "react";
+
+//Framer Motion
+import { motion } from "framer-motion";
+
+//Component
 const CircleComponent = () => {
+  //States
+  //Window Size
+  const [windowSize, setWindowSize] = useState<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
 
-    //drag constraint calculator based on window size
+  //LifeCycle Management
+  useEffect(() => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
 
-    //random number generator for positioning of balls
+  //Refs
+  //Circle
+  const circle = useRef(null)
 
-
+  //JSX
   return (
-    <motion.div drag initial={{ scale: 0 }} whileHover={{ scale: 1.1 }}
-    animate={{ scale: 1 }} whileDrag={{ scale: 0.9 }}
-    className=" rounded-full w-32 h-32 p-4 shadow-2xl
-    text-green-200 bg-slate-600 right-5 top-20 bg-opacity-80 relative right "  >
+    <motion.div
+      ref={ circle }
+      drag
+      initial={{ scale: 0 }}
+      whileHover={{ scale: 1.1 }}
+      animate={{ scale: 1 }}
+      whileDrag={{ scale: 0.9 }}
+      dragConstraints={{ right: 0 }}
+      className=" rounded-full w-32 h-32 p-4 shadow-2xl
+    text-green-200 bg-slate-600 right-5 top-20 bg-opacity-80 relative right "
+    ></motion.div>
+  );
+};
 
-    </motion.div>
-  )
-}
-
-export default CircleComponent
+export default CircleComponent;
