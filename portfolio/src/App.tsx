5ca970896f9b885adab7a7b3ec2a8f54fde5b539
Cimorexave <sadeq.s502@gmail.com>
2022-09-10 21:36:31 +0430

    feat: added expanding context throughout the component tree

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -6,19 +6,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Components & Routes
 import Home from "./routes/Home";
 //Context
-import { dragContext } from "./utils/context";
+import { dragContext, expandContext } from "./utils/context";
 
 const App = () => {
   //context state
   const [isDragging, setIsDragging]  = useState<any>();
+  const [isExpanding, setIsExpanding] = useState<any>();
 
   return (
     <BrowserRouter>
+    <expandContext.Provider value={{ isExpanding, setIsExpanding }}>
       <dragContext.Provider value={{ isDragging, setIsDragging }}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </dragContext.Provider>
+    </expandContext.Provider>
     </BrowserRouter>
   );
 };
