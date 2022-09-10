2b3bc4d867d9226efebe52293f59fcc9b1378f20
Cimorexave <sadeq.s502@gmail.com>
2022-09-10 21:34:15 +0430

    feat: created expanding context

--- portfolio/src/utils/context.tsx
+++ portfolio/src/utils/context.tsx
@@ -4,8 +4,16 @@ export interface dragContextInterface {
   isDragging: boolean;
   setIsDragging: Function;
 }
-
+export interface expandContextInterface {
+  isExpanding: boolean;
+  setIsExpanding: Function;
+}
 export const dragContext = React.createContext<dragContextInterface>({
   isDragging: false,
   setIsDragging: () => {},
 });
+
+export const expandContext = React.createContext<expandContextInterface>({
+  isExpanding: false,
+  setIsExpanding: () => {},
+})
